881ad3d21558c7cdbc40ffc9a1a3e15b1e7cca06
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2022-10-24 02:03:27 +0800

    Update bgjj.js

--- bgjj.js
+++ bgjj.js
@@ -10,7 +10,7 @@ TG频道群：https://t.me/gjwj6
 
 [rewrite_local]
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/bgjj.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/gjwj666/qx/main/bgjj.js
 
 [mitm] 
 
