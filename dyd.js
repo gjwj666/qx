818531d36fc653b824e146f5d4e4b01197b8c7c3
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2022-10-24 00:25:40 +0800

    Update dyd.js

--- dyd.js
+++ dyd.js
@@ -16,8 +16,7 @@ tehvh.cmmsjbu.com
 
 
 var gjwj = $request.headers;
-
-     gjwj['authorization'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJwdWJsaWMiLCJleHAiOjE2NjkwNTMzNDIsImlzc3VlciI6ImNvbS5idXR0ZXJmbHkiLCJzdWIiOiJhc2lnbiIsInVzZXJJZCI6NjYzNzUwNH0.7ZAMUsP8TKCe8TqhpNMZfzhOuKWnAVm39s1XFHxlozA';
-
-
-$done({headers : gjwj}); 
+gjwj['Authorization'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJwdWJsaWMiLCJleHAiOjE2NjkwNTMzNDIsImlzc3VlciI6ImNvbS5idXR0ZXJmbHkiLCJzdWIiOiJhc2lnbiIsInVzZXJJZCI6NjYzNzUwNH0.7ZAMUsP8TKCe8TqhpNMZfzhOuKWnAVm39s1XFHxlozA';
+$done({
+    headers: gjwj
+});
