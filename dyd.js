b2a7e9a3a96558d01c32be626cf0ff64f99462ea
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2022-10-23 23:31:06 +0800

    Create dyd.js

---
+++ dyd.js
@@ -0,0 +1,23 @@
+
+/*
+
+#懂阴帝
+
+https:\/\/tehvh\.cmmsjbu\.com.+ url request-header dyd.js
+
+主机名MITM
+
+*.cmmsjbu.*
+
+
+
+*/
+
+
+
+var gjwj = $request.headers;
+
+     gjwj['authorization'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJwdWJsaWMiLCJleHAiOjE2NjkwNTMzNDIsImlzc3VlciI6ImNvbS5idXR0ZXJmbHkiLCJzdWIiOiJhc2lnbiIsInVzZXJJZCI6NjYzNzUwNH0.7ZAMUsP8TKCe8TqhpNMZfzhOuKWnAVm39s1XFHxlozA';
+
+
+$done({headers : gjwj}); 
