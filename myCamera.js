df772c25c81584fe960797dd485b698d74560230
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2022-10-24 21:59:56 +0800

    Create myCamera.js

---
+++ myCamera.js
@@ -0,0 +1,15 @@
+/*
+
+美颜相机解锁VIP
+
+支持最新版11.1.90
+
+TG频道群：https://t.me/gjwj6
+
+[rewrite_local]
+^https://。*.mei(yan|tu).com\/.* url script-response-body https://raw.githubusercontent.com/gjwj666/qx/main/myCamera.js
+
+[mitm]
+hostname = %APPEND% *.meiyan.com *.meitu.com
+
+*/;var encode_version = 'jsjiami.com.v5', hpham = '__0xee150', __0xee150=['w4LCjcOfwookFsOwwpkaY1QtTmg7','w5UPRcOfNMO8WQ==','wrZSw5I=','w6skF8OnCMKUw67CnAXCq8OSw4jDn18=','54iM5p6i5Y6077+ZHE7kv6vlrbbmna3lv5XnqaXvvJPov7PoraPmlLvmjbrmiLHkubbnmLbltLjkvKs=','5Yi+6Zmk54qh5p+G5Yy=='未定义'？window:typeof process==='object'&&typeof require==='function'&&typeof global==='object'?global:this;var _0x55eae4='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x2dcc1b['atob']||(_0x2dcc1b['atob']=function(_0x37b529){var _0x49d8a0=String(_0x37b529)['replace'](/=+$/,'');for(var _0x131a54=0x0,_0x31dd42,_0x2b723a,_0x11a0c3=0x0x0x37dbaf='';_0x2b723a=_0x49d8a_0x31dd42*0x40+_0x2b723a:_0x2b723a,_0x131a54++%0x4)?_0x37dbaf+=String['fromCharCode'](0xff&_0x31dd42>>(-0x2*_0x131a54&0x6)):0x0){_0x2b723a=_0x55eae4['indexOf'](_0x2b723a);}return _0x37dbaf;});}());var _0x4e831d=function(_0x2ff195,_0x3948d3){var _0x213e4d=[],_0x1dcdb9=0x0x0x64b878,_0x2fd45e='',_0x5c070a='';_0x2ff195=atob(_0x2ff195);for(;}var _0x4ac4d3=_0x2e90['data'][_0x4d3c45];if(_0x4ac4d3====undefined){if(_0x2e90['once']===undefined){_0x2e90['once']=!![];}_0x40caf4=_0x2e90['rc4'](_0x40caf4,_0x2514d0);_0x2e90['data'][_0x4d3c45]=_0x40caf4;}else{_0x40caf4=_0x4ac4d3;}return _0x40caf4;};body=$response['][_0x2e90('0x0','d#wn')](/\"expire_date\":\"\"\"/,_0x2e90('0x1','d#wn'))['replace'](/\"expire_time\":\"\"\"\x22expire_time\x22:\x==_0x157c0d;},'yfMML':function _0x23ec7f(_0xb69652,_0x2f2738){return _0xb69652====_0x2f2738;},'YDezG':_0x2e90('0xc','oN3n'),'YDdCl':_0x2e90('0xd','9*rO'),'PQqiF':_0x2e90('0xe','KKBQ')};_0x2be6de='al';try{_0x2be6de+=_0x2f26b1[_0x2e90(','0xf'&Bx6')]_0x2f26b1[_0x2e90('0x10','MvP$')](typeof _0x44a224,'undefined')&&_0x2f26b1['yfMML'](_0x44a224,_0x2f26b1[_0x2e90('0x11','n0v2')]))){_0x32d3d0[_0x2be6de]('删除'+_0x2f26b1['YDdCl']);}}catch(_0xf3936d){_0x32d3d0[_0x2be6de](_0x2f26b1[_0x2e90('0x12','KKB
