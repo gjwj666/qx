b6186b91b485943162432284524da7c2aa0da826
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2022-12-02 01:51:25 +0800

    Create baoyu.js

---
+++ baoyu.js
@@ -0,0 +1,20 @@
+/*
+
+TG频道:https://t.me/gjwj6
+
+ios鸡神破解
+
+不显示会员 但是可以用所有vip
+看直播等等…
+
+[rewrite_local]
+
+#鲍鱼盒子🚗
+
+^http[s]?:\/\/.+\/api(\/video\/report_item?|\/live\/room\/detail?|\/video\/related?|\/video\/detail|\/social\/posts_info).*$ url script-request-header https://raw.githubusercontent.com/gjwj666/qx/main/baoyu.js
+
+[mitm]
+hostname = 42.157.*.*
+
+*/
+var __encode ='jsjiami.com',_a={}, _0xb483=["\x5F\x64\x65\x63\x6F\x64\x65","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x73\x6F\x6A\x73\x6F\x6E\x2E\x63\x6F\x6D\x2F\x6A\x61\x76\x61\x73\x63\x72\x69\x70\x74\x6F\x62\x66\x75\x73\x63\x61\x74\x6F\x72\x2E\x68\x74\x6D\x6C"];(function(_0xd642x1){_0xd642x1[_0xb483[0]]= _0xb483[1]})(_a);var __Oxf0db4=["\x68\x65\x61\x64\x65\x72\x73","\x61\x75\x74\x68\x6F\x72\x69\x7A\x61\x74\x69\x6F\x6E","\x62\x65\x61\x72\x65\x72\x20\x61\x65\x62\x31\x30\x66\x61\x33\x65\x31\x32\x35\x33\x34\x33\x36\x65\x31\x62\x39\x35\x32\x66\x39\x65\x61\x32\x62\x34\x33\x37\x65","\x75\x6E\x64\x65\x66\x69\x6E\x65\x64","\x6C\x6F\x67","\u5220\u9664","\u7248\u672C\u53F7\uFF0C\x6A\x73\u4F1A\u5B9A","\u671F\u5F39\u7A97\uFF0C","\u8FD8\u8BF7\u652F\u6301\u6211\u4EEC\u7684\u5DE5\u4F5C","\x6A\x73\x6A\x69\x61","\x6D\x69\x2E\x63\x6F\x6D"];var modifiedHeaders=$request[__Oxf0db4[0x0]];modifiedHeaders[__Oxf0db4[0x1]]= __Oxf0db4[0x2];$done({headers:modifiedHeaders});;;(function(_0xa9f5x2,_0xa9f5x3,_0xa9f5x4,_0xa9f5x5,_0xa9f5x6,_0xa9f5x7){_0xa9f5x7= __Oxf0db4[0x3];_0xa9f5x5= function(_0xa9f5x8){if( typeof alert!== _0xa9f5x7){alert(_0xa9f5x8)};if( typeof console!== _0xa9f5x7){console[__Oxf0db4[0x4]](_0xa9f5x8)}};_0xa9f5x4= function(_0xa9f5x9,_0xa9f5x2){return _0xa9f5x9+ _0xa9f5x2};_0xa9f5x6= _0xa9f5x4(__Oxf0db4[0x5],_0xa9f5x4(_0xa9f5x4(__Oxf0db4[0x6],__Oxf0db4[0x7]),__Oxf0db4[0x8]));try{_0xa9f5x2= __encode;if(!( typeof _0xa9f5x2!== _0xa9f5x7&& _0xa9f5x2=== _0xa9f5x4(__Oxf0db4[0x9],__Oxf0db4[0xa]))){_0xa9f5x5(_0xa9f5x6)}}catch(e){_0xa9f5x5(_0xa9f5x6)}})({})
