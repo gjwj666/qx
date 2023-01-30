ff6db6a83c944e82198aa9851804096d96c4499c
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2023-01-30 19:11:51 +0800

    Update xbc.js

--- xbc.js
+++ xbc.js
@@ -31,6 +31,9 @@ hostname= api101.xbcapp.xyz
 var gjwj = $request.headers;
 
      gjwj['x-api-key'] = '5sx_rJM7wTzbfQE1VuOeaIpT7SzU-jP4_1675072798';
+     gjwj['device-id'] = 'a254b22e173042d1dcba46b093b8425b';
+     gjwj['mc'] =
+'TIiSmFFoTZfMsxvCYZRODJaDqFTyP9Y5S6wxt74BLn8%3D';
      gjwj['User-Agent'] =
 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1';
 $done({headers : gjwj}); 
