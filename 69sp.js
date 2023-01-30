3b7ebeec93b49db92f8121da9f4c45fd7fae8876
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2023-01-30 14:37:03 +0800

    Update 69sp.js

--- 69sp.js
+++ 69sp.js
@@ -11,8 +11,9 @@ TG频道群：https://t.me/gjwj6
 *******************************/
 
 
-[rewrite_local]
+
 https:\/\/jiekou1\.shijiujin\.com\/Login\/info url script-response-body https://raw.githubusercontent.com/gjwj666/qx/main/69sp.js
+
 
 [mitm] 
 hostname = *.shijiujin.*
