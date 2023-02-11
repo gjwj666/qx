3d87cc552e45983271d8f2c9544e7c6877678af8
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2023-02-12 07:41:21 +0800

    Update qyxjvip.js

--- qyxjvip.js
+++ qyxjvip.js
@@ -10,6 +10,12 @@ hostname = commerce-api.faceu.mobi
 *******************************/
 
 var modifiedHeaders=$request['headers'];
-modifiedHeaders['x-Tt-Token']='00a57ade3b97e8c4a2d921fb60b2f70b3a0410cd62ff31669bfd0741d756dee74438654e4582dd8436304dfd4ea006335caae44ff4e737584a8a465da9a79276760e7c3099838990eca235136270a8d77b4b375e07bbb63bcc94e9995056fa8dff964-1.0.1';
-modifiedHeaders['User-Agent']='Beauty 5.1.0 rv:5.1.0.14 (iPhone; iOS 14.4; zh_CN) Cronet';
+modifiedHeaders['x-Tt-Token']= '00a57ade3b97e8c4a2d921fb60b2f70b3a0410cd62ff31669bfd0741d756dee74438654e4582dd8436304dfd4ea006335caae44ff4e737584a8a465da9a79276760e7c3099838990eca235136270a8d77b4b375e07bbb63bcc94e9995056fa8dff964-1.0.1';
+modifiedHeaders['User-Agent']= 'Beauty 5.1.0 rv:5.1.0.14 (iPhone; iOS 14.4; zh_CN) Cronet';
+modifiedHeaders['device-type']= '';
+modifiedHeaders['X-Gorgon']= '';
+modifiedHeaders['X-Argus']= '';
+modifiedHeaders['X-Khronos']= '';
+modifiedHeaders['X-Ladon']= '';
+
 $done({'headers':modifiedHeaders});
