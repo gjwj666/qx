159cc544c9554e65601ebfe2bd50119ea287b420
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2023-02-12 00:18:40 +0800

    Update jiuy.js

--- jiuy.js
+++ jiuy.js
@@ -4,7 +4,7 @@
 
 [rewrite_local]
 
-https:\/\/29xx\.top\/index\/user\/receipt.*$ url script-request-header https://raw.githubusercontent.com/gjwj666/qx/main/jiuy.js
+https:\/\/29xx\.top\/index\/user\/receipt.*$ url script-response-body https://raw.githubusercontent.com/gjwj666/qx/main/jiuy.js
 
 [mitm] 
 
@@ -12,12 +12,10 @@ hostname = 29xx.top
 
 *******************************/
 
-var modifiedHeaders=$request['headers'];
-modifiedHeaders['appid']='8857065256369871';
-modifiedHeaders['accessToken']='be8c7d41e6dbbfdbada7de061f45fa32';
-modifiedHeaders['token']='778555';
-modifiedHeaders['User-Agent']='Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1';
-modifiedHeaders['request-token']='cc01129c1706f7d3208506164d0299f79e1a16eb';
-$done({
-    'headers': modifiedHeaders
-});
+var obj = JSON.parse($response.body); 
+
+obj = {
+  ZMsIJUsHQFgja6M+sgjDWKOW2c5YvdzMuUgmkfs/94lCM2XNJvXOyMfJ8+0dg0kFEjMfdvZoTH79PToRNQoYNydKv7fvOY4VCJIK/Rp2aU/BFhrPOnFyYnFFqPQNkvDOi4pCqaM/pT+lmaDVufJx5cTNkJh88PmEte4Uq1AMMakHOM1GD1CqWleKWM/ZTONIXdtKZyc8CVMwI/9jd6OSkIzI+rDrJq/qQTcHqAwz3O5q1R1LQFtgWl10eHgdya3rG4V30dHevUEC8SZg2FQ2sFeZ9vQVllmPb0bbs5ErtTLPnssCbSw6quSTJLomhKqsEeLmG8IScYQf+jVQnh2HOQFBpCMWI/lQudXOVY42EfyMdm7GWhZpfXDGLCRJr4x09A83f4z1Tw9l1daRgFKHCjrSgcJQ4mihB8p2DIQSeuje4nOiW43EZZmG+cAl9LUdKVHJqkeRttFCnZcKSapYmSZ5rdMFz6cYFt9obHL0Srsq6IFZSci7xaGCeRF2vTdb8+KXlvsF8PJCjuh2RFFv6LfYCHJ/D0Uw00b14fheUmKWFjdqSw6asUhw41uhPAN9umUc4nvFtrTtqieS/2pghW3YiTt3XhnAOvtpFzF+fzk19bHNsZ+Bs65Qnjrk0bBmTHdk/ehvg3Octarf7N7MiFIGhyMF8cgDnRrAI9yxhdVMWj9u+juduXIaNhDMJ8e7L2r0tN8LyNAw4xbcNAneCatgYKt1peHcsKslERR39e/WpNkd0D5orn/HrgXHHmnrD9YC93PrcnrUoKPwrhL4dk6RUI+wwvuZeOlYIy/xSvzjaCjYOVJft6vyiem2hIZHQcR0vXe7y899K39zDhmTQDNAlqQJ33+WSV7IZLT1fk5wuctxgJrAl7fxlcjAt49cY2WEbcEECK/zgNZ/7py8A36364AwR/6pm7YUYwNM+jpZHx0CJoCyHkNqmgipqy5XR8idC10OvO89rEW0
+}
+$done({body:JSON.stringify(obj)});
+// 
