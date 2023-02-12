334e7192842ed6feae2f4ec6811075e7d03bbbfb
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2023-02-12 15:20:29 +0800

    Update and rename jy.js to JY.js

--- JY.js
+++ JY.js
@@ -14,7 +14,7 @@ TG频道群：https://t.me/gjwj6
 
 [rewrite_local]
 
-^http[s]?:\/\/commerce-api-.*\.faceu\.mobi\/commerce\/v1\/.*$ url script-request-header https://raw.githubusercontent.com/gjwj666/qx/main/jy.js
+^http[s]?:\/\/commerce-api-.*\.faceu\.mobi\/commerce\/v1\/.*$ url script-request-header https://raw.githubusercontent.com/gjwj666/qx/main/JY.js
 
 [mitm] 
 
