48375cd48b4bd544d7a14f8346747c5f4bc695b5
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2023-02-17 16:21:20 +0800

    Update 91putao.js

--- 91putao.js
+++ 91putao.js
@@ -7,7 +7,7 @@
 
 [rewrite_local]
 
-https:\/\/gateway\.h84ug5\.com\/member\/memberInfo\/.*$ url script-request-header https://raw.githubusercontent.com/gjwj666/qx/main/91putao.js
+^http[s]?:\/\/gateway\.h84ug5\.com\/member\/memberInfo\/.*$ url script-request-header https://raw.githubusercontent.com/gjwj666/qx/main/91putao.js
 
 [mitm] 
 
@@ -16,4 +16,5 @@ hostname = *.h84ug5.*
 *******************************/
 var modifiedHeaders=$request['headers'];
 modifiedHeaders['Authorization']= 'eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoxMTA0NzAyLCJ1c2VyX2tleSI6ImVlODkzNmU0LTExMzQtNGI0OS04OTY2LTIzMDlkNDcxMGZiYiJ9.bbd5bYbjZe80ZXZP7XB-lbHthE6EmTuWalZpaVS9PAJMV8ClN1RNYon4j1jqPyh0-75kxXoSZUYHRx0ZgYlZFA';
+modifiedHeaders['User-Agent']= '365Live/2023.02.16 CFNetwork/1220.1 Darwin/20.3.0';
 $done({'headers':modifiedHeaders});
