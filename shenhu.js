c264b340e943b2d872eb557baf35472806f4614b
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2023-02-21 20:38:16 +0800

    Create shenhu.js

---
+++ shenhu.js
@@ -0,0 +1,17 @@
+/******************************
+
+[rewrite_local]
+
+^http[s]?:\/\/api\.domain\.liuniukeji\.heshuangde\.com\/index\.php\/App.*$ url script-request-header shenhu.js
+
+[mitm] 
+
+hostname = api.domain.liuniukeji.heshuangde.com
+
+*******************************/
+var modifiedHeaders=$request['headers'];
+modifiedHeaders['user-info']= '100018106';
+modifiedHeaders['uid']= '100018106';
+modifiedHeaders['system']= '{"name":"aVBob25l","systemName":"iOS","systemVersion":"14.4","model":"iPhone","localizedModel":"iPhone","identifierForVendor":"935CD856-9A2C-4D14-80E4-C821F25B93B0","isPhysicalDevice":true,"isTF":"1","bundleID":"jp.kobe.liveapp"}';
+modifiedHeaders['token']= 'ecbcbff74c05224cc4f46926477f0e28';
+$done({'headers':modifiedHeaders});
