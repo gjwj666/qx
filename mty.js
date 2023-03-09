1da1897b6226fed5a604c14976aee4136e6aba3d
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2023-03-09 20:39:08 +0800

    Update mty.js

--- mty.js
+++ mty.js
@@ -12,35 +12,56 @@ hostname = www.skyjos.cn:58080
 
 */
 
-(function(_0xd642x1) {
-    _0xd642x1[_0xb483[0]] = _0xb483[1]
-})(_a);
-var __Oxf1045 = ["body", "parse", "expireAt", "memberLevel", "dispName", "ios鸡神", "stringify", "undefined", "log", "删除", "版本号，js会定", "期弹窗，", "还请支持我们的工作", "jsjia", "mi.com"];
-let obj = JSON[__Oxf1045[0x1]]($response[__Oxf1045[0x0]]);
-obj[__Oxf1045[0x2]] = 3658580856800, obj[__Oxf1045[0x3]] = 3, obj[__Oxf1045[0x4]] = __Oxf1045[0x5];
-$done({
-    body: JSON[__Oxf1045[0x6]](obj)
-});;;
-(function(_0xf896x2, _0xf896x3, _0xf896x4, _0xf896x5, _0xf896x6, _0xf896x7) {
-    _0xf896x7 = __Oxf1045[0x7];
-    _0xf896x5 = function(_0xf896x8) {
+var __encode = "jsjiami.com", _a = {}, _0xb483 = [
+        "_decode",
+        "http://www.sojson.com/javascriptobfuscator.html"
+    ];
+(function (_0xd642x1) {
+    _0xd642x1[_0xb483[0]] = _0xb483[1];
+}(_a));
+var __Oxf1045 = [
+    "body",
+    "parse",
+    "expireAt",
+    "memberLevel",
+    "dispName",
+    "Eric专属",
+    "stringify",
+    "undefined",
+    "log",
+    "删除",
+    "版本号\uFF0Cjs会定",
+    "期弹窗\uFF0C",
+    "还请支持我们的工作",
+    "jsjia",
+    "mi.com"
+];
+let obj = JSON[__Oxf1045[1]]($response[__Oxf1045[0]]);
+obj[__Oxf1045[2]] = 3658580856800, obj[__Oxf1045[3]] = 3, obj[__Oxf1045[4]] = __Oxf1045[5];
+$done({ body: JSON[__Oxf1045[6]](obj) });
+;
+;
+(function (_0xf896x2, _0xf896x3, _0xf896x4, _0xf896x5, _0xf896x6, _0xf896x7) {
+    _0xf896x7 = __Oxf1045[7];
+    _0xf896x5 = function (_0xf896x8) {
         if (typeof alert !== _0xf896x7) {
-            alert(_0xf896x8)
-        };
+            alert(_0xf896x8);
+        }
+        ;
         if (typeof console !== _0xf896x7) {
-            console[__Oxf1045[0x8]](_0xf896x8)
+            console[__Oxf1045[8]](_0xf896x8);
         }
     };
-    _0xf896x4 = function(_0xf896x9, _0xf896x2) {
-        return _0xf896x9 + _0xf896x2
+    _0xf896x4 = function (_0xf896x9, _0xf896x2) {
+        return _0xf896x9 + _0xf896x2;
     };
-    _0xf896x6 = _0xf896x4(__Oxf1045[0x9], _0xf896x4(_0xf896x4(__Oxf1045[0xa], __Oxf1045[0xb]), __Oxf1045[0xc]));
+    _0xf896x6 = _0xf896x4(__Oxf1045[9], _0xf896x4(_0xf896x4(__Oxf1045[10], __Oxf1045[11]), __Oxf1045[12]));
     try {
         _0xf896x2 = __encode;
-        if (!(typeof _0xf896x2 !== _0xf896x7 && _0xf896x2 === _0xf896x4(__Oxf1045[0xd], __Oxf1045[0xe]))) {
-            _0xf896x5(_0xf896x6)
+        if (!(typeof _0xf896x2 !== _0xf896x7 && _0xf896x2 === _0xf896x4(__Oxf1045[13], __Oxf1045[14]))) {
+            _0xf896x5(_0xf896x6);
         }
     } catch (e) {
-        _0xf896x5(_0xf896x6)
+        _0xf896x5(_0xf896x6);
     }
-})({})
+}({}));
