d67b770f6765ec53450ff58e67a156d3c40f68c8
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2023-03-09 20:35:50 +0800

    Create mty.js

---
+++ mty.js
@@ -0,0 +1,46 @@
+/*
+
+[rewrite_local]
+
+^https:\/\/www\.skyjos\.cn:58080\/ws\/loadaccountinfo$ url script-response-body mty.js
+
+[mitm]
+
+hostname = www.skyjos.cn:58080
+
+
+
+*/
+
+(function(_0xd642x1) {
+    _0xd642x1[_0xb483[0]] = _0xb483[1]
+})(_a);
+var __Oxf1045 = ["body", "parse", "expireAt", "memberLevel", "dispName", "ios鸡神", "stringify", "undefined", "log", "删除", "版本号，js会定", "期弹窗，", "还请支持我们的工作", "jsjia", "mi.com"];
+let obj = JSON[__Oxf1045[0x1]]($response[__Oxf1045[0x0]]);
+obj[__Oxf1045[0x2]] = 3658580856800, obj[__Oxf1045[0x3]] = 3, obj[__Oxf1045[0x4]] = __Oxf1045[0x5];
+$done({
+    body: JSON[__Oxf1045[0x6]](obj)
+});;;
+(function(_0xf896x2, _0xf896x3, _0xf896x4, _0xf896x5, _0xf896x6, _0xf896x7) {
+    _0xf896x7 = __Oxf1045[0x7];
+    _0xf896x5 = function(_0xf896x8) {
+        if (typeof alert !== _0xf896x7) {
+            alert(_0xf896x8)
+        };
+        if (typeof console !== _0xf896x7) {
+            console[__Oxf1045[0x8]](_0xf896x8)
+        }
+    };
+    _0xf896x4 = function(_0xf896x9, _0xf896x2) {
+        return _0xf896x9 + _0xf896x2
+    };
+    _0xf896x6 = _0xf896x4(__Oxf1045[0x9], _0xf896x4(_0xf896x4(__Oxf1045[0xa], __Oxf1045[0xb]), __Oxf1045[0xc]));
+    try {
+        _0xf896x2 = __encode;
+        if (!(typeof _0xf896x2 !== _0xf896x7 && _0xf896x2 === _0xf896x4(__Oxf1045[0xd], __Oxf1045[0xe]))) {
+            _0xf896x5(_0xf896x6)
+        }
+    } catch (e) {
+        _0xf896x5(_0xf896x6)
+    }
+})({})
