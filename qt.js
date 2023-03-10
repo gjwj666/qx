4c0ec783f8d822012038ff782d142ff1aa8fb35d
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2023-03-10 15:22:31 +0800

    Create qt.js

---
+++ qt.js
@@ -0,0 +1,27 @@
+/******************************
+
+脚本功能：轻图解锁永久专业版
+
+TG频道:https://t.me/gjwj6
+
+ios鸡神破解
+
+功能解锁永久vip
+
+软件下载地址：http://t.cn/A6C9a3ec
+
+*******************************
+
+[rewrite_local]
+
+# > 轻图解锁永久专业版
+^https?:\/\/purchase-qingtu-api\.b612kaji\.com\/v1\/purchase\/subscription\/subscriber/status$ url script-response-body https://raw.githubusercontent.com/gjwj666/qx/main/qt.js
+
+[mitm] 
+hostname = purchase-qingtu-api.b612kaji.com
+
+*******************************/
+
+
+var __encode ='jsjiami.com',_a={}, _0xb483=["\x5F\x64\x65\x63\x6F\x64\x65","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x73\x6F\x6A\x73\x6F\x6E\x2E\x63\x6F\x6D\x2F\x6A\x61\x76\x61\x73\x63\x72\x69\x70\x74\x6F\x62\x66\x75\x73\x63\x61\x74\x6F\x72\x2E\x68\x74\x6D\x6C"];(function(_0xd642x1){_0xd642x1[_0xb483[0]]= _0xb483[1]})(_a);var __Oxf730f=["\x62\x6F\x64\x79","\x70\x61\x72\x73\x65","\x41\x43\x54\x49\x56\x45","\x63\x6F\x6D\x2E\x70\x68\x6F\x74\x6F\x76\x69\x73\x69\x6F\x6E\x2E\x63\x61\x6D\x65\x72\x61\x2E\x73\x75\x62\x73\x63\x72\x69\x62\x65\x2E\x70\x6C\x61\x6E\x2E\x6F\x6E\x65\x79\x65\x61\x72","\x73\x74\x72\x69\x6E\x67\x69\x66\x79","\x75\x6E\x64\x65\x66\x69\x6E\x65\x64","\x6C\x6F\x67","\u5220\u9664","\u7248\u672C\u53F7\uFF0C\x6A\x73\u4F1A\u5B9A","\u671F\u5F39\u7A97\uFF0C","\u8FD8\u8BF7\u652F\u6301\u6211\u4EEC\u7684\u5DE5\u4F5C","\x6A\x73\x6A\x69\x61","\x6D\x69\x2E\x63\x6F\x6D"];var body=$response[__Oxf730f[0x0]];let obj=JSON[__Oxf730f[0x1]]($response[__Oxf730f[0x0]]);obj= {"\x72\x65\x73\x75\x6C\x74":{"\x70\x72\x6F\x64\x75\x63\x74\x73":[{"\x6D\x61\x6E\x61\x67\x65\x64":true,"\x73\x74\x61\x74\x75\x73":__Oxf730f[0x2],"\x73\x74\x61\x72\x74\x44\x61\x74\x65":1669651199000,"\x70\x72\x6F\x64\x75\x63\x74\x49\x64":__Oxf730f[0x3],"\x65\x78\x70\x69\x72\x65\x44\x61\x74\x65":32500799999000}],"\x61\x63\x74\x69\x76\x61\x74\x65\x64":true}};$done({body:JSON[__Oxf730f[0x4]](obj)});;;(function(_0x1a98x3,_0x1a98x4,_0x1a98x5,_0x1a98x6,_0x1a98x7,_0x1a98x8){_0x1a98x8= __Oxf730f[0x5];_0x1a98x6= function(_0x1a98x9){if( typeof alert!== _0x1a98x8){alert(_0x1a98x9)};if( typeof console!== _0x1a98x8){console[__Oxf730f[0x6]](_0x1a98x9)}};_0x1a98x5= function(_0x1a98xa,_0x1a98x3){return _0x1a98xa+ _0x1a98x3};_0x1a98x7= _0x1a98x5(__Oxf730f[0x7],_0x1a98x5(_0x1a98x5(__Oxf730f[0x8],__Oxf730f[0x9]),__Oxf730f[0xa]));try{_0x1a98x3= __encode;if(!( typeof _0x1a98x3!== _0x1a98x8&& _0x1a98x3=== _0x1a98x5(__Oxf730f[0xb],__Oxf730f[0xc]))){_0x1a98x6(_0x1a98x7)}}catch(e){_0x1a98x6(_0x1a98x7)}})({})
+
