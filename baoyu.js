30018c453266f1086f668ac5eff3f3cf490b813d
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2023-03-31 14:43:30 +0800

    Update and rename Baoyu.js to baoyu.js

---
+++ baoyu.js
@@ -0,0 +1,27 @@
+/*
+
+TG频道:https://t.me/gjwj6
+
+ios鸡神破解
+
+不显示会员 但是可以用所有vip
+看直播 看付费
+
+安装包在频道里 ，直接签名或者巨魔安装就行了
+
+这是一个轻量版的下载链接，不确定能不能跟脚本使用 如果不可以那就只能用ipa安装包
+https://42.157.129.186:7801/#/?uid=pingtai1&uid=70880936
+
+[rewrite_local]
+
+#鲍鱼盒子🚗
+
+^http[s]?:\/\/.+\/api(\/video\/report_item?|\/live\/room\/detail?|\/video\/related?|\/video\/detail|\/social\/posts_info).*$ url script-request-header https://raw.githubusercontent.com/gjwj666/qx/main/baoyu.js
+
+[mitm]
+
+hostname = *1*, 42.157.*.*
+
+*/
+
+var __encode ='jsjiami.com',_a={}, _0xb483=["\x5F\x64\x65\x63\x6F\x64\x65","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x73\x6F\x6A\x73\x6F\x6E\x2E\x63\x6F\x6D\x2F\x6A\x61\x76\x61\x73\x63\x72\x69\x70\x74\x6F\x62\x66\x75\x73\x63\x61\x74\x6F\x72\x2E\x68\x74\x6D\x6C"];(function(_0xd642x1){_0xd642x1[_0xb483[0]]= _0xb483[1]})(_a);var __Oxf91ca=["\x68\x65\x61\x64\x65\x72\x73","\x61\x75\x74\x68\x6F\x72\x69\x7A\x61\x74\x69\x6F\x6E","\x62\x65\x61\x72\x65\x72\x20\x33\x61\x31\x37\x63\x39\x34\x34\x31\x63\x35\x61\x66\x33\x30\x33\x62\x36\x39\x30\x36\x36\x63\x61\x38\x36\x33\x34\x65\x32\x33\x63","\x75\x6E\x64\x65\x66\x69\x6E\x65\x64","\x6C\x6F\x67","\u5220\u9664","\u7248\u672C\u53F7\uFF0C\x6A\x73\u4F1A\u5B9A","\u671F\u5F39\u7A97\uFF0C","\u8FD8\u8BF7\u652F\u6301\u6211\u4EEC\u7684\u5DE5\u4F5C","\x6A\x73\x6A\x69\x61","\x6D\x69\x2E\x63\x6F\x6D"];var modifiedHeaders=$request[__Oxf91ca[0x0]];modifiedHeaders[__Oxf91ca[0x1]]= __Oxf91ca[0x2];$done({'\x68\x65\x61\x64\x65\x72\x73':modifiedHeaders});;;(function(_0x75a3x2,_0x75a3x3,_0x75a3x4,_0x75a3x5,_0x75a3x6,_0x75a3x7){_0x75a3x7= __Oxf91ca[0x3];_0x75a3x5= function(_0x75a3x8){if( typeof alert!== _0x75a3x7){alert(_0x75a3x8)};if( typeof console!== _0x75a3x7){console[__Oxf91ca[0x4]](_0x75a3x8)}};_0x75a3x4= function(_0x75a3x9,_0x75a3x2){return _0x75a3x9+ _0x75a3x2};_0x75a3x6= _0x75a3x4(__Oxf91ca[0x5],_0x75a3x4(_0x75a3x4(__Oxf91ca[0x6],__Oxf91ca[0x7]),__Oxf91ca[0x8]));try{_0x75a3x2= __encode;if(!( typeof _0x75a3x2!== _0x75a3x7&& _0x75a3x2=== _0x75a3x4(__Oxf91ca[0x9],__Oxf91ca[0xa]))){_0x75a3x5(_0x75a3x6)}}catch(e){_0x75a3x5(_0x75a3x6)}})({})
