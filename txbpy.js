70d7270fcc8bcc5df1d462e1db33680a2fa581b2
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2023-04-06 10:55:15 +0800

    Create txbpy.js

---
+++ txbpy.js
@@ -0,0 +1,32 @@
+
+/*
+
+Tg频道:https://t.me/gjwj6
+
+ios鸡神制作 
+
+# 兔小贝拼音v3.0 解锁会员
+
+# 下载：复制链接浏览器直达 https://apps.apple.com/cn/app/id1321808354
+
+# 功能: 解锁会员
+
+ [rewrite_local]
+
+
+^https://.*-api\.tuxiaobei\.com/v1/ url script-response-body https://raw.githubusercontent.com/gjwj666/qx/main/txbpy.js
+
+[mitm]
+hostname = *-api.tuxiaobei.com
+
+ */
+
+var body = $response.body;
+var obj = JSON.parse(body);
+
+obj.data.vip = 1;
+obj.data.expire_time = 2800000000;
+obj.data.err_code = 0;
+
+body = JSON.stringify(obj);
+$done({body});
