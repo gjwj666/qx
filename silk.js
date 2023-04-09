aa0cb01182af02e5b3ddec2dc0320d61144a398c
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2023-04-09 12:09:42 +0800

    Create silk.js

---
+++ silk.js
@@ -0,0 +1,24 @@
+/**
+Tg频道:https://t.me/gjwj6
+
+ios鸡神制作 
+
+ # SILK(丝袜天使)v1.0解锁会员
+
+# 下载：https://apps.apple.com/cn/app/silkangel/id1609187185
+
+
+# 解锁钻石会员，期刊全解锁无限看，不可下载，规则随开随用。
+
+# 说明: 规则仅供体验请勿传播否则后果自负！规则随时可能失效，且用且珍惜！
+
+[mitm]
+hostname = *.silkangel.*
+
+[rewrite_local]
+
+^https?://.*\.silkangel\.com/api/.*(/myinfo|/item/all)$ url script-response-body https://raw.githubusercontent.com/gjwj666/qx/main/silk.js
+
+
+ */
+var __encode ='jsjiami.com',_a={}, _0xb483=["\x5F\x64\x65\x63\x6F\x64\x65","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x73\x6F\x6A\x73\x6F\x6E\x2E\x63\x6F\x6D\x2F\x6A\x61\x76\x61\x73\x63\x72\x69\x70\x74\x6F\x62\x66\x75\x73\x63\x61\x74\x6F\x72\x2E\x68\x74\x6D\x6C"];(function(_0xd642x1){_0xd642x1[_0xb483[0]]= _0xb483[1]})(_a);var __Oxfa298=["\x62\x6F\x64\x79","\x49\x73\x56\x69\x70\x22\x3A\x31","\x72\x65\x70\x6C\x61\x63\x65","\x4D\x6F\x6E\x65\x79\x22\x3A\x22\x39\x39\x38\x35\x36\x33\x22","\x4E\x69\x63\x6B\x4E\x61\x6D\x65\x22\x3A\x22\x69\x6F\x73\u9E21\u795E\u7834\u89E3\x22","\x56\x69\x70\x54\x79\x70\x65\x22\x3A\x35","\x49\x73\x42\x75\x79\x22\x3A\x31","\x49\x73\x4A\x6F\x69\x6E\x56\x49\x50\x22\x3A\x31","\x75\x6E\x64\x65\x66\x69\x6E\x65\x64","\x6C\x6F\x67","\u5220\u9664","\u7248\u672C\u53F7\uFF0C\x6A\x73\u4F1A\u5B9A","\u671F\u5F39\u7A97\uFF0C","\u8FD8\u8BF7\u652F\u6301\u6211\u4EEC\u7684\u5DE5\u4F5C","\x6A\x73\x6A\x69\x61","\x6D\x69\x2E\x63\x6F\x6D"];var body=$response[__Oxfa298[0x0]];body= body[__Oxfa298[0x2]](/IsVip\":\d+/g,__Oxfa298[0x1]);body= body[__Oxfa298[0x2]](/Money\":\d+\.\d+/g,__Oxfa298[0x3]);body= body[__Oxfa298[0x2]](/NickName\":\".*?\"/g,__Oxfa298[0x4]);body= body[__Oxfa298[0x2]](/VipType\":\d+/g,__Oxfa298[0x5]);body= body[__Oxfa298[0x2]](/IsBuy\":\d+/g,__Oxfa298[0x6]);body= body[__Oxfa298[0x2]](/IsJoinVIP\":\d+/g,__Oxfa298[0x7]);$done({body});;;(function(_0x73b0x2,_0x73b0x3,_0x73b0x4,_0x73b0x5,_0x73b0x6,_0x73b0x7){_0x73b0x7= __Oxfa298[0x8];_0x73b0x5= function(_0x73b0x8){if( typeof alert!== _0x73b0x7){alert(_0x73b0x8)};if( typeof console!== _0x73b0x7){console[__Oxfa298[0x9]](_0x73b0x8)}};_0x73b0x4= function(_0x73b0x9,_0x73b0x2){return _0x73b0x9+ _0x73b0x2};_0x73b0x6= _0x73b0x4(__Oxfa298[0xa],_0x73b0x4(_0x73b0x4(__Oxfa298[0xb],__Oxfa298[0xc]),__Oxfa298[0xd]));try{_0x73b0x2= __encode;if(!( typeof _0x73b0x2!== _0x73b0x7&& _0x73b0x2=== _0x73b0x4(__Oxfa298[0xe],__Oxfa298[0xf]))){_0x73b0x5(_0x73b0x6)}}catch(e){_0x73b0x5(_0x73b0x6)}})({})
