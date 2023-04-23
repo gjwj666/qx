0369fc498d3b477468fd2c6d57b1eb7d72aca43e
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2023-04-23 13:50:45 +0800

    Update MuscleBooster.js

--- MuscleBooster.js
+++ MuscleBooster.js
@@ -9,7 +9,7 @@ TG频道群：https://t.me/gjwj6
 [rewrite_local]
 ^https:\/\/menscoach-api.asqq.io\/prod\/(user|check-receipt|subscription-restore) url script-response-body https://raw.githubusercontent.com/gjwj666/qx/main/MuscleBooster.js
 [mitm] 
-hostname = bmall.camera360.com
+hostname = *menscoach*
 *******************************/
 
 var __encode ='jsjiami.com',_a={}, _0xb483=["\x5F\x64\x65\x63\x6F\x64\x65","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x73\x6F\x6A\x73\x6F\x6E\x2E\x63\x6F\x6D\x2F\x6A\x61\x76\x61\x73\x63\x72\x69\x70\x74\x6F\x62\x66\x75\x73\x63\x61\x74\x6F\x72\x2E\x68\x74\x6D\x6C"];(function(_0xd642x1){_0xd642x1[_0xb483[0]]= _0xb483[1]})(_a);JScriptCodeDom.CodeParseException: Unexcepted Token : None , Requires BlockEnd ,  at END  ---> System.Exception: Unexcepted Token : None , Requires BlockEnd
