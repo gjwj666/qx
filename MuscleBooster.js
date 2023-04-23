ac555960003ef17997d902f35b140780eeddc6ad
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2023-04-23 14:38:11 +0800

    Update MuscleBooster.js

--- MuscleBooster.js
+++ MuscleBooster.js
@@ -12,17 +12,40 @@ TG频道群：https://t.me/gjwj6
 hostname = *menscoach*
 *******************************/
 
-var __encode ='jsjiami.com',_a={}, _0xb483=["\x5F\x64\x65\x63\x6F\x64\x65","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x73\x6F\x6A\x73\x6F\x6E\x2E\x63\x6F\x6D\x2F\x6A\x61\x76\x61\x73\x63\x72\x69\x70\x74\x6F\x62\x66\x75\x73\x63\x61\x74\x6F\x72\x2E\x68\x74\x6D\x6C"];(function(_0xd642x1){_0xd642x1[_0xb483[0]]= _0xb483[1]})(_a);JScriptCodeDom.CodeParseException: Unexcepted Token : None , Requires BlockEnd ,  at END  ---> System.Exception: Unexcepted Token : None , Requires BlockEnd
-   at JScriptCodeDom.CodeParser.ReadAndCheckToken(CodeTokenType equalto)
-   at JScriptCodeDom.CodeParser.ParseToBlock(CodeBlock block)
-   at JScriptCodeDom.CodeParser.ParseIfStatement()
-   at JScriptCodeDom.CodeParser.InternalParseStatementCore()
-   at JScriptCodeDom.CodeParser.InternalParseStatement()
-   at JScriptCodeDom.CodeParser.ParseStatement()
-   at JScriptCodeDom.CodeParser.ParseBlock()
-   at JScriptCodeDom.CodeParser.Parse(String code, String filename, Int32 lineoffset, Boolean keeplinenum)
-   --- End of inner exception stack trace ---
-   at JScriptCodeDom.CodeParser.Parse(String code, String filename, Int32 lineoffset, Boolean keeplinenum)
-   at ProtectorV1.Protect(String[] codes)
-   at ProtectorV1.Protect(String code)
-   at ASP.javascript_obfuscator_aspx.Button1_Click(Object sender, EventArgs e)
+var body = $response.body;
+var url = $request.url;
+var parse = JSON.parse(body);
+var is_validation_success = /"is_validation_success":\s+\w+/g;
+var is_paid = /"is_paid":\s+\w+/g;
+var name = /"name":\s+".*?"/g;
+
+if (url.indexOf("subscription-restore") != -1) {
+
+body = body.replace(is_validation_success,'"is_validation_success":true');
+
+$notify("-Muscle Booster 🏊","*************","永久vip破解成功‼️");
+
+$done({
+    body
+});
+}
+
+if (url.indexOf("user") != -1) {
+
+body = body.replace(is_paid,'"is_paid":true').replace(name,'"name":"iOs鸡神🐔"');
+
+$done({
+    body
+});
+}
+
+if (url.indexOf("check-receipt") != -1) {
+
+body = body.replace(is_paid,'"is_paid":true').replace(name,'"name":"Ios鸡神🐔"');
+
+$notify("-Muscle Booster 🏊","*************","永久vip破解成功‼️");
+
+$done({
+    body
+});
+}
