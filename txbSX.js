fd43013373336d5489378fbdf3590d4f9760bc67
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2023-06-06 09:25:54 +0800

    Update and rename txbsx.js to txbSX.js

--- txbSX.js
+++ txbSX.js
@@ -1,6 +1,6 @@
 /**
 
-Tg频道:https://t.me/gjwj6
+Tg频道:https://t.me/iosapp520
 
 ios鸡神制作 
 
@@ -11,7 +11,7 @@ ios鸡神制作
 
  [rewrite_local]
 
-^https://.*-api\.tuxiaobei\.com/v1/user/ url script-response-body https://raw.githubusercontent.com/gjwj666/qx/main/txbsx.js
+^https://.*-api\.tuxiaobei\.com/v1/user/ url script-response-body https://raw.githubusercontent.com/gjwj666/qx/main/txbSX.js
 
 [mitm]
 hostname = *-api.tuxiaobei.com
