1200233cb79195667a7996189c06a0452bbedbe1
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2023-08-13 13:43:28 +0800

    Update and rename llsyd.js to liulishuo.js

--- liulishuo.js
+++ liulishuo.js
@@ -4,7 +4,7 @@
 
 下载地址：https://is.gd/ANNpWW
 
-TG频道群：https://t.me/gjwj6
+TG频道群：https://t.me/iosapp
 
 使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
@@ -12,7 +12,7 @@ TG频道群：https://t.me/gjwj6
 
 [rewrite_local]
 
-^https?:\/\/vira\.llsapp\.com\/api\/v\d\/\w+\/\w+$ url script-response-body https://raw.githubusercontent.com/gjwj666/qx/main/llsyd.js
+^https?:\/\/vira\.llsapp\.com\/api\/v\d\/\w+\/\w+$ url script-response-body https://raw.githubusercontent.com/gjwj666/qx/main/liulishuo.js
 
 
 [mitm] 
