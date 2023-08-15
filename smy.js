ee65e0d8597de0419ae7fb308c3a161ac39a9b00
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2023-08-15 13:10:10 +0800

    Update and rename jssmy.js to smy.js

--- smy.js
+++ smy.js
@@ -4,7 +4,7 @@
 
 下载地址：https://is.gd/898e5E
 
-TG频道群：https://t.me/gjwj6
+TG频道群：https://t.me/iosapp520
 
 使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
@@ -12,7 +12,7 @@ TG频道群：https://t.me/gjwj6
 
 [rewrite_local]
 
-^https:\/\/scanner\.jianse\.tv\/api\/users\/loginUser url script-response-body https://raw.githubusercontent.com/gjwj666/qx/main/jssmy.js
+^https:\/\/scanner\.jianse\.tv\/api\/users\/loginUser url script-response-body https://raw.githubusercontent.com/gjwj666/qx/main/smy.js
 
 [mitm] 
 
