11b11bf3339f2b23120a6ba557f081d206a0d072
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2023-08-31 15:54:09 +0800

    Update and rename madousp.js to mdsp.js

--- mdsp.js
+++ mdsp.js
@@ -2,13 +2,13 @@
  
 Tg频道:https://t.me/iosapp520
 
-脚本来自𝒀𝒖𝒉𝒆𝒏𝒈修改而成 
+脚本来自𝒀𝒖𝒉𝒆𝒏𝒈佬修改而成 如有疑问 联系删除
 
 下载地址:https://uu.z78l8cz.cc/?pc=79PJZ52K
 
 解锁射区永久全能卡 观看付费视频
 
-http[s]?://.*(bytebwq|qrneryt|dirptev|saejeuj).com/api/app/(ping/config|media/(play|homev2|short/recommend)|user/info) url script-response-body https://raw.githubusercontent.com/gjwj666/qx/main/madousp.js
+http[s]?://.*(bytebwq|qrneryt|dirptev|saejeuj).com/api/app/(ping/config|media/(play|homev2|short/recommend)|user/info) url script-response-body https://raw.githubusercontent.com/gjwj666/qx/main/mdsp.js
 
 hostname=*bytebwq.com,*qrneryt.com,*dirptev.com,*saejeuj.com
 
