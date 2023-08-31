c0e5512b7b6ef34caa479e03864e661d7618ca2c
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2023-08-31 15:44:14 +0800

    Update and rename madou.js to madoushiping.js

---
+++ madoushiping.js
@@ -0,0 +1,15 @@
+/******************************************
+ 
+Tg频道:https://t.me/iosapp520
+
+脚本来自𝒀𝒖𝒉𝒆𝒏𝒈修改而成 
+
+下载地址:https://uu.z78l8cz.cc/?pc=79PJZ52K
+
+解锁射区永久全能卡 观看付费视频
+
+http[s]?://.*(bytebwq|qrneryt|dirptev|saejeuj).com/api/app/(ping/config|media/(play|homev2|short/recommend)|user/info) url script-response-body https://raw.githubusercontent.com/gjwj666/qx/main/madou.js
+
+hostname=*bytebwq.com,*qrneryt.com,*dirptev.com,*saejeuj.com
+
+******************************************/
