63c0706f981cea34a1cf66987c48883279882b65
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2023-10-09 13:14:10 +0800

    Update bhjk.js
    更新body

--- bhjk.js
+++ bhjk.js
@@ -24,16 +24,66 @@ Tg频道 https://t.me/iosapp520
 hostname = api.boohee.com
 
 */
+let body = $response.body;
+let obj = JSON.parse(body);
 
-var obj = JSON.parse($response.body);
+obj.data = {
+  "code": 0,
+  "message": "0",
+  "now": 1696828109,
+  "data": {
+    "user": {
+      "user_key": "072ea330-fd27-4059-8866-8266eb2dddb9",
+      "user_name": "BH969368620",
+      "cellphone": "132****5223",
+      "email": "",
+      "role": 0,
+      "user_type": 1,
+      "cellphone_state": true,
+      "gender": "",
+      "birthday": "2000-01-01",
+      "height": 0,
+      "avatar_url": "https://one.boohee.cn/beauty/uploads/asserts/20200819114402/default_female_avatar.png"
+    },
+    "vip": {
+      "is_member": true,
+      "state": "1",
+      "remain_day": 0,
+      "started_at": "",
+      "expired_at": "2199-01-01 00:00:00",
+      "integral_member": {
+        "integral": 0,
+        "level": 1,
+        "level_name": "蓝钻会员"
+      },
+      "app_vip_banner_url": "https://up.boohee.cn/house/u/one/easy_member/homepage_ad2.png",
+      "notices_tips": ["购买年会员券额返券", "联合会员买一得三"],
+      "member_center_url": "https://member.boohee.com/easy/index",
+      "member_active_url": "https://member.boohee.com/easy/active",
+      "member_sale_url": "https://member.boohee.com/easy/introduce",
+      "union_member_url": "https://member.boohee.com/associate-member/member-index",
+      "super_member_url": "https://member.boohee.com/super-member",
+      "common_member_url": "https://member.boohee.com/multiply-channel",
+      "bind_cellphone_url": "https://member.boohee.com/easy/index",
+      "colum_list": [{
+        "title": "智能方案",
+        "sub_title": "量身定制专属饮食方案",
+        "link_url": "https://member.boohee.com/member-sale?type=intelligent&page_from=App_mine"
+      }, {
+        "title": "饮食建议",
+        "sub_title": "多维度营养分析 实时点评&建议",
+        "link_url": "https://member.boohee.com/member-sale?type=diet&page_from=App_mine"
+      }],
+      "icon_title": "了解更多",
+      "not_vip_tips": "量身定制方案，助力轻松减脂",
+      "notices": []
+    },
+    "find_tab_ab_test": 1,
+    "is_new_weight": true
+  }
+};
 
-if ($request.url.indexOf('api.boohee.com/app-interface/v1/user/user_info') !== -1) {
-    obj.member = true;
-    obj.state = "1";
-    obj.expired_at = "2199-01-01 00:00:00";
-}
-
-$done({body: JSON.stringify(obj)});
+$done({ body: JSON.stringify(obj) });
 
 
 
