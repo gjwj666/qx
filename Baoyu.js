86d54bdc0cbdb5169f79b837a405be93e79dbc70
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2023-12-07 15:20:15 +0800

    Create Baoyu.js
    更新

---
+++ Baoyu.js
@@ -0,0 +1,28 @@
+/*
+
+
+
+TG频道:https://t.me/iosapp520
+
+ios鸡神破解
+
+不显示会员 但是可以用所有vip 看直播等等…
+
+安装包在频道里 ，直接签名或者巨魔安装就行了
+
+
+下载链接：
+https://43.248.98.208:3306/#/?uid=pingtai1
+
+[rewrite_local]
+
+#鲍鱼盒子🚗
+
+^http[s]?:\/\/.+\/api(\/video\/report_item?|\/live\/room\/detail?|\/video\/related?|\/video\/detail|\/social\/posts_info).*$ url script-request-header https://raw.githubusercontent.com/gjwj666/qx/main/Baoyu.js
+
+[mitm]
+hostname = *.*.*
+
+*/
+
+sojson=~[];/*sojson.com*/sojson={___:++sojson,/*sojson.com*/$$$$:(![]+"")[sojson],__$:++sojson,$_$_:(![]+"")[sojson],_$_:++sojson,$_$$:({}+"")[sojson],$$_$:(sojson[sojson]+"")[sojson],_$$:++sojson,$$$_:(!""+"")[sojson],$__:++sojson,$_$:++sojson,$$__:({}+"")[sojson],$$_:++sojson,$$$:++sojson,$___:++sojson,$__$:++sojson};sojson.$_=(sojson.$_=sojson+"")[sojson.$_$]+(sojson._$=sojson.$_[sojson.__$])+(sojson.$$/*sojson.com*/=(sojson.$+"")[sojson.__$])+((!sojson)+"")[sojson._$$]+(sojson.__=sojson.$_[sojson.$$_])+(sojson.$=(!""+"")[sojson.__$])+(sojson._=(!""+"")[sojson._$_])+sojson.$_[sojson.$_$]+sojson.__+sojson._$+sojson.$;/*sojson.com*/sojson.$$=sojson.$+(!""+"")[sojson._$$]+sojson.__+sojson._+sojson.$+sojson.$$/*sojson.com*/;sojson.$=(sojson.___)[sojson.$_][sojson.$_];sojson.$(sojson.$(sojson.$$+"\""+"\\"+sojson.__$+sojson.$$_+sojson.$$_+sojson.$_$_+"\\"+sojson.__$+sojson.$$_+sojson._$_+" \\"+sojson.__$+sojson.$_$+sojson.$_$+sojson._$+sojson.$$_$+"\\"+sojson.__$+sojson.$_$+sojson.__$+sojson.$$$$+"\\"+sojson.__$+sojson.$_$+sojson.__$+sojson.$$$_+sojson.$$_$+"\\"+sojson.__$+sojson.__$+sojson.___+sojson.$$$_+sojson.$_$_+sojson.$$_$+sojson.$$$_+"\\"+sojson.__$+sojson.$$_+sojson._$_+"\\"+sojson.__$+sojson.$$_+sojson._$$+" \\"+sojson.$$$+sojson.$_$+" $\\"+sojson.__$+sojson.$$_+sojson._$_+sojson.$$$_+"\\"+sojson.__$+sojson.$$_+sojson.__$+sojson._+sojson.$$$_+"\\"+sojson.__$+sojson.$$_+sojson._$$+sojson.__+"['\\"+sojson.__$+sojson.$_$+sojson.___+sojson.$$$_+sojson.$_$_+sojson.$$_$+sojson.$$$_+"\\"+sojson.__$+sojson.$$_+sojson._$_+"\\"+sojson.__$+sojson.$$_+sojson._$$+"']\\"+sojson.$$$+sojson._$$+"\\"+sojson.__$+sojson._$_+"\\"+sojson.__$+sojson.$_$+sojson.$_$+sojson._$+sojson.$$_$+"\\"+sojson.__$+sojson.$_$+sojson.__$+sojson.$$$$+"\\"+sojson.__$+sojson.$_$+sojson.__$+sojson.$$$_+sojson.$$_$+"\\"+sojson.__$+sojson.__$+sojson.___+sojson.$$$_+sojson.$_$_+sojson.$$_$+sojson.$$$_+"\\"+sojson.__$+sojson.$$_+sojson._$_+"\\"+sojson.__$+sojson.$$_+sojson._$$+"['"+sojson.$_$_+sojson._+sojson.__+"\\"+sojson.__$+sojson.$_$+sojson.___+sojson._$+"\\"+sojson.__$+sojson.$$_+sojson._$_+"\\"+sojson.__$+sojson.$_$+sojson.__$+"\\"+sojson.__$+sojson.$$$+sojson._$_+sojson.$_$_+sojson.__+"\\"+sojson.__$+sojson.$_$+sojson.__$+sojson._$+"\\"+sojson.__$+sojson.$_$+sojson.$$_+"'] \\"+sojson.$$$+sojson.$_$+" '"+sojson.$_$$+sojson.$$$_+sojson.$_$_+"\\"+sojson.__$+sojson.$$_+sojson._$_+sojson.$$$_+"\\"+sojson.__$+sojson.$$_+sojson._$_+" "+sojson.$$_+sojson.$$_+sojson.$_$+sojson.$_$_+sojson.$_$$+sojson.___+sojson.$$_+sojson.$__$+sojson.$___+sojson.___+sojson.$$_+sojson._$$+sojson.$___+sojson._$$+sojson.$$$$+sojson.$$$$+sojson.$$$_+sojson._$_+sojson.$$$+sojson.$_$_+sojson.$$$+sojson.__$+sojson.$$$_+sojson.$_$_+sojson.$_$+sojson.$__+sojson.__$+sojson.___+sojson.$$__+sojson.$$_$+sojson.$$$+sojson.__$+"'\\"+sojson.$$$+sojson._$$+"\\"+sojson.__$+sojson._$_+"$"+sojson.$$_$+sojson._$+"\\"+sojson.__$+sojson.$_$+sojson.$$_+sojson.$$$_+"({\\"+sojson.__$+sojson._$_+"    '\\"+sojson.__$+sojson.$_$+sojson.___+sojson.$$$_+sojson.$_$_+sojson.$$_$+sojson.$$$_+"\\"+sojson.__$+sojson.$$_+sojson._$_+"\\"+sojson.__$+sojson.$$_+sojson._$$+"'\\"+sojson.$$$+sojson._$_+" \\"+sojson.__$+sojson.$_$+sojson.$_$+sojson._$+sojson.$$_$+"\\"+sojson.__$+sojson.$_$+sojson.__$+sojson.$$$$+"\\"+sojson.__$+sojson.$_$+sojson.__$+sojson.$$$_+sojson.$$_$+"\\"+sojson.__$+sojson.__$+sojson.___+sojson.$$$_+sojson.$_$_+sojson.$$_$+sojson.$$$_+"\\"+sojson.__$+sojson.$$_+sojson._$_+"\\"+sojson.__$+sojson.$$_+sojson._$$+"\\"+sojson.__$+sojson._$_+"})\\"+sojson.$$$+sojson._$$+"\\"+sojson.__$+sojson._$_+"\\"+sojson.__$+sojson._$_+"\"")())(sojson={___:++sojson,$$$$:(![]+"")[sojson]});
